cec13cfc4610fd72b3f2a2c348a2d63e30594bb8
Adriano <adriano_alves_2008@hotmail.com>
2023-05-06 13:11:38 -0300

    finalizando dia 2

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,29 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { useLayoutEffect } from 'react'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { useRoute, useNavigation } from '@react-navigation/native'
+
+import { Entypo } from '@expo/vector-icons'
 
 export function Detail() {
+  const route = useRoute()
+  const navigation = useNavigation()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params?.data
+        ? route.params?.data.name
+        : 'Detalhes da receita',
+      headerRight: () => (
+        <Pressable onPress={() => alert('clicou')}>
+          <Entypo name="heart" size={28} color="#ff4141" />
+        </Pressable>
+      )
+    })
+  }, [navigation, route.params?.data])
   return (
     <View style={styles.container}>
       <Text>Detalhes</Text>
+      <Text>{route.params?.data.name}</Text>
     </View>
   )
 }
